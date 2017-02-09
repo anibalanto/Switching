64463afbb532a873455512ec460c04ddc22d2075
Anibal Fernando Antonelli <anibal.antonelli@fi.mdp.edu.ar>
2017-02-09 15:36:32 -0300

    Cambio Anibal
    asdff

--- Codes/GeneraLogisB2.cpp
+++ Codes/GeneraLogisB2.cpp
@@ -1,4 +1,4 @@
-//#include <stdio.h>  //sprintf, FILE
+//#include <stdio.h>  //sprintf, FILE ;aosdifh
 //#include <string.h> //strcpy, strcat
 //#include <math.h>   //pow, floor
 #include "maxiutils.h"  //tiene los cuantificadores
@@ -6,7 +6,7 @@
 #include <random>
 #include <conio.h> //getch, solo para MS-DOS
 
-using namespace std; // Agrego el espacio de nombres del estandar de C++, zafo de poner std:: adelante de cada lÌnea.
+using namespace std; // Agrego el espacio de nombres del estandar de C++, zafo de poner std:: adelante de cada l√≠nea.
 
 int main()
 {
@@ -14,30 +14,30 @@ int main()
         int NBases = (sizeof(Bases)/sizeof(Bases[0]));
     int Precisions[] = {1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52}; //Contiene todas las precisiones que voy barriendo
         int NPrecisions = (sizeof(Precisions)/sizeof(Precisions[0])); // Cantidad de precisiones
-    double Margins[2] = {0, 1}; //Los m·rgenes de la PDF-Val
+    double Margins[2] = {0, 1}; //Los m√°rgenes de la PDF-Val
     unsigned long int NInitialConditions = 100; // Es la cantidad de condiciones iniciales diferentes de los que se larga el atractor.
     unsigned long int NIter = 1e7; // Es el largo de cada atractor
 
     unsigned long int Bins = 1024; // Cantidad de bines del histograma
-    unsigned long int DimEmb = 6; // DimensiÛn de embedding para MP, BP y BPW
+    unsigned long int DimEmb = 6; // Dimensi√≥n de embedding para MP, BP y BPW
 
     double* Map; //Declare the pointer
         Map = (double*) malloc (sizeof(double) * (NIter + 1)); //Creates the array. It has one more postition at first for the length
         Map[0] = (double)NIter;
 
-    char StrAux[32]; // Ac· armo los nombres de los archivos
+    char StrAux[32]; // Ac√° armo los nombres de los archivos
     double Scale; // Es la escala que utilizo para multiplicar y dividir en el floor
-    double InvScale; // Guardo ac· la inversa de la escala para cambiar una divisiÛn por una multiplicaciÛn en cada iteraciÛn del mapa
+    double InvScale; // Guardo ac√° la inversa de la escala para cambiar una divisi√≥n por una multiplicaci√≥n en cada iteraci√≥n del mapa
 
     double Hval, Qval, Cval, Hbp, Qbp, Cbp, Hbpw, Qbpw, Cbpw, MP, Period; // Vectores en donde van guardados los cuantificadores
 
     double InitialConditions[NInitialConditions]; // Declaro el vector de condiciones iniciales
     random_device rd;
-    mt19937_64 gen(rd()); // Creo un objeto del tipo mt19937 en presiciÛn double con semilla en 0
-    uniform_real_distribution <double> dis(0, 1); // Defino el tipo de distribuciÛn
+    mt19937_64 gen(rd()); // Creo un objeto del tipo mt19937 en presici√≥n double con semilla en 0
+    uniform_real_distribution <double> dis(0, 1); // Defino el tipo de distribuci√≥n
     for (unsigned long int iInitialCondition = 0; iInitialCondition < NInitialConditions; iInitialCondition++) // Lleno un vector de condiciones iniciales
     {
-        InitialConditions[iInitialCondition] = dis(gen); // Sortea el primer valor del mapa, es una variable uniformemente distribuÌda entre 0 y 1
+        InitialConditions[iInitialCondition] = dis(gen); // Sortea el primer valor del mapa, es una variable uniformemente distribu√≠da entre 0 y 1
     }
     printf("Generadas %ld condiciones iniciales\n\n", NInitialConditions);
 
@@ -61,38 +61,38 @@ int main()
 
                 printf("\t\tCondicion inicial %d/%d = %.32f\n", (int)iInitialCondition+1, (int)NInitialConditions, Map[1]); // Para debuguear
 
-                for (unsigned long int iMap = 1; iMap < NIter; iMap++) // Va riterando el mapa logÌstico
+                for (unsigned long int iMap = 1; iMap < NIter; iMap++) // Va riterando el mapa log√≠stico
                 {
-                    Map[iMap+1] =  4*InvScale*floor(Scale*Map[iMap]*(1-Map[iMap])); // Mapa logÌstico, x[n] = r*x[n-1]*(1-x[n-1]), caÛtico con r=4. Ni la resta ni la multiplicaciÛn por un entero generan fraccionarios
-                } // Ac· ya tengo el atractor guardado en el vector Map
+                    Map[iMap+1] =  4*InvScale*floor(Scale*Map[iMap]*(1-Map[iMap])); // Mapa log√≠stico, x[n] = r*x[n-1]*(1-x[n-1]), ca√≥tico con r=4. Ni la resta ni la multiplicaci√≥n por un entero generan fraccionarios
+                } // Ac√° ya tengo el atractor guardado en el vector Map
 
-                double* PDFval = PDF_val(Map, Bins, Margins, "normalyzed"); // Genera el histograma de patrones de Ûrden
-                Hval = entropy(PDFval, "normalyzed"); // Le calcula la entropÌa y la suma para el promedio
+                double* PDFval = PDF_val(Map, Bins, Margins, "normalyzed"); // Genera el histograma de patrones de √≥rden
+                Hval = entropy(PDFval, "normalyzed"); // Le calcula la entrop√≠a y la suma para el promedio
                 Qval = disequilibrum(PDFval, "normalyzed"); // Le calcula el desequilibrio
                 Cval = Hval*Qval; // Le calcula la complejidad
                 free(PDFval); // Libera el vector que contiene al histograma
 
-                double* PDFbp = PDF_BP(Map, DimEmb, "overlapped", "normalyzed"); // Genera el histograma de patrones de Ûrden
-                Hbp = entropy(PDFbp, "normalyzed"); // Le calcula la entropÌa
+                double* PDFbp = PDF_BP(Map, DimEmb, "overlapped", "normalyzed"); // Genera el histograma de patrones de √≥rden
+                Hbp = entropy(PDFbp, "normalyzed"); // Le calcula la entrop√≠a
                 Qbp = disequilibrum(PDFbp, "normalyzed"); // Le calcula el desequilibrio
                 Cbp = Hbp*Qbp; // Le calcula la complejidad
                 MP = missing_patterns(PDFbp);
                 free(PDFbp); // Libera el vector que contiene al histograma
 
-                double* PDFbpw = PDF_BPW(Map, DimEmb, "overlapped"); // Genera el histograma de patrones de Ûrden
-                Hbpw = entropy(PDFbpw, "normalyzed"); // Le calcula la entropÌa
+                double* PDFbpw = PDF_BPW(Map, DimEmb, "overlapped"); // Genera el histograma de patrones de √≥rden
+                Hbpw = entropy(PDFbpw, "normalyzed"); // Le calcula la entrop√≠a
                 Qbpw = disequilibrum(PDFbpw, "normalyzed"); // Le calcula el desequilibrio
                 Cbpw = Hbpw*Qbpw; // Le calcula la complejidad
                 free(PDFbpw); // Libera el vector que contiene al histograma
 
-                Period = find_period(Map, 1); //Para mapas unidimensionales, para los switch voy a tener que usar dimensiÛn 2.
+                Period = find_period(Map, 1); //Para mapas unidimensionales, para los switch voy a tener que usar dimensi√≥n 2.
 
                 sprintf(StrAux, "B%d_P%d_CI%d", Bases[iBases], Precisions[iPrecisions],iInitialCondition);
-                fprintf(ResultsLog,"%s\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\n", StrAux, Hval, Qval, Cval, Hbp, Qbp, Cbp, Hbpw, Qbpw, Cbpw, MP, Period); //Guarda los valores en el archivo de salida, escribo la condiciÛn inicial para evaluar el comportamiento del rand()
+                fprintf(ResultsLog,"%s\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\t%.8e\n", StrAux, Hval, Qval, Cval, Hbp, Qbp, Cbp, Hbpw, Qbpw, Cbpw, MP, Period); //Guarda los valores en el archivo de salida, escribo la condici√≥n inicial para evaluar el comportamiento del rand()
             }
         }
     }
     fclose(ResultsLog); // Cierra el archivo de salida, queda un archivo por base para este oscilador
     printf("\nPresionar una tecla");
-    getch(); //Avisa que terminÛ y espera una tecla
+    getch(); //Avisa que termin√≥ y espera una tecla
 }
